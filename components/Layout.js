9c6dc1b9e8240ab367a97d99f89e58d8e87ff466
Amalazing <lawrencemueller18@gmail.com>
2021-08-10 19:49:39 -0700

    Added the banner for the home page

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,14 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 
 import styles from '@/styles/Layout.module.css';
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -14,6 +18,15 @@ export default function Layout({ title, keywords, description, children }) {
       </Head>
 
       <Header />
+
+      {/* The following bit of logic is conditional rendering
+          in order to render the Hero Showcase Banner.
+          This comment is to note that the double ampersand is
+          basically a shorthand for else. So the following line
+          reads if pathname is equal to home then render showcase
+      */}
+      {router.pathname === '/' && <Showcase />}
+
       <div className={styles.container}>{children}</div>
       <Footer />
     </div>
---
+++ components/Showcase.js
@@ -0,0 +1,10 @@
+import styles from '@/styles/Showcase.module.css';
+
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Help fight for the environment!</h1>
+      <h2>Find sustainability events</h2>
+    </div>
+  );
+}
