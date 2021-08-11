c1eb46d3f2a99f466d1008026e75b969c3fe6fb3
Amalazing <lawrencemueller18@gmail.com>
2021-08-10 17:37:56 -0700

    initial commit, mostly boilerplate

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>App to find latest sustainability events</p>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
