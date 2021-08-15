825d9eae8ca820f6e9bee4570fb2e6bb6201122e
Amalazing <lawrencemueller18@gmail.com>
2021-08-15 14:38:27 -0700

    I now have dynamic paths based on dummy data and a completely functioning front end. It looks fine for now, time to move onto the back end.

--- pages/api/events/[slug].js
+++ pages/api/events/[slug].js
@@ -1,7 +1,7 @@
 const { events } = require('./data.json');
 
 export default function handler(req, res) {
-  const evt = events.filter((ev) => ev.slug === req.query.slug); //Create a variable that is equal to the single event determined by slug
+  const evt = events.filter((ev) => ev.slug === req.query.slug);
 
   if (req.method === 'GET') {
     res.status(200).json(evt);
@@ -9,5 +9,4 @@ export default function handler(req, res) {
     res.setHeader('Allow', ['GET']);
     res.status(405).json({ message: `Method ${req.method} is not allowed` });
   }
-  res.status(200).json(events);
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,23 @@
 import Layout from '@/components/Layout';
+import Link from 'next/link';
 import { API_URL } from '@/config/index';
+import EventItem from '@/components/EventItem';
 
-export default function Home() {
+export default function Home({ events }) {
   return (
     <Layout>
       <h1>A few Upcoming Events</h1>
+      {events.length === 0 && <h3>No Events to show</h3>}
+
+      {events.map((evt) => (
+        <EventItem key={evt.id} evt={evt} />
+      ))}
+
+      {events.length > 0 && (
+        <Link href='/events'>
+          <a className='btn-secondary'>View All</a>
+        </Link>
+      )}
     </Layout>
   );
 }
@@ -14,7 +27,7 @@ export async function getStaticProps() {
   const events = await res.json();
 
   return {
-    props: { events },
+    props: { events: events.slice(0, 3) },
     revalidate: 1,
   };
 }
