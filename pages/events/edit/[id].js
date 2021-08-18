ca0b782d32d7b7d7fc2dddf961d5c7c26afda2db
Amalazing <lawrencemueller18@gmail.com>
2021-08-17 22:06:09 -0700

    created the image upload component and built out the functionality. Seems to be working well and saving the image to strapi and cloudinary

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -7,6 +7,7 @@ import { useRouter } from 'next/router';
 import Link from 'next/link';
 import Image from 'next/image';
 import { API_URL } from '@/config/index.js';
+import ImageUpload from '@/components/ImageUpload';
 import Layout from '@/components/Layout';
 import Modal from '@/components/Modal';
 import styles from '@/styles/Form.module.css';
@@ -61,6 +62,14 @@ export default function EditEventPage({ evt }) {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setValues({ ...values, [name]: value });
+  };
+
+  const imageUploaded = async (e) => {
+    const res = await fetch(`${API_URL}/events/${evt.id}`);
+    const data = await res.json();
+
+    setImagePreview(data.image.formats.thumbnail.url);
+    setShowModal(false);
   };
 
   return (
@@ -166,7 +175,7 @@ export default function EditEventPage({ evt }) {
       </div>
 
       <Modal show={showModal} onClose={() => setShowModal(false)}>
-        Image Upload
+        <ImageUpload evtId={evt.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
   );
