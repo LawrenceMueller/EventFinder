89a9453dd279c4672f8963810720751eccad06fa
Amalazing <lawrencemueller18@gmail.com>
2021-08-26 16:58:39 -0700

    Decided on a theme for the site, socializing your pet dog. Since this is just a portfolio site, the theme matters less than the functionality, but at the same time I feel like this theme is unique.

--- components/Showcase.js
+++ components/Showcase.js
@@ -3,8 +3,8 @@ import styles from '@/styles/Showcase.module.css';
 export default function Showcase() {
   return (
     <div className={styles.showcase}>
-      <h1>Help fight for the environment!</h1>
-      <h2>Find sustainability events</h2>
+      <h1>Social Pup</h1>
+      <h2>The easiest way to socialize your dog!</h2>
     </div>
   );
 }
--- pages/about.js
+++ pages/about.js
@@ -4,7 +4,12 @@ export default function AboutPage() {
   return (
     <Layout title='Sustainable Events | About'>
       <h1>About</h1>
-      <p>App to find latest sustainability events</p>
+      <p>
+        App that helps dog owners find groups near them in order to properly
+        socialize thier dogs. Socializing teaches your dog how to react to the
+        world around them in a healthy way, without unnecessary fear or
+        aggression.
+      </p>
       <p>Version: 1.0.0</p>
     </Layout>
   );
